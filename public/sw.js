34e3e6809bdbcba89460adae3f6e256050debebc
AlexFoxJS <alexfoxjs@gmail.com>
2018-11-11 14:29:53 +0300

    master: 31 Updating  Activating Service Workers

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,16 @@
-//
+// https://developer.mozilla.org/en-US/docs/Web/API/InstallEvent
 self.addEventListener('install', event => {
 	console.log('[Service Worker] Install Service Worker ...', event)
 });
-//
+
+// https://developer.mozilla.org/en-US/docs/Web/Events/activate
 self.addEventListener('activate', event => {
-	console.log('[Service Worker] Activating Service Worker ...', event)
+	console.log('[Service Worker] Activating Service Worker ...', event);
 	return self.clients.claim()
+});
+
+// https://developer.mozilla.org/en-US/docs/Web/API/FetchEvent
+self.addEventListener('fetch', event => {
+	console.log('[Service Worker] Fetching something ...', event);
+	event.respondWith(fetch(event.request));
 });
