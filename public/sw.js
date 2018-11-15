12ad30834acaf27701796408a72d682075b87196
AlexFoxJS <alexfoxjs@gmail.com>
2018-11-15 04:02:33 +0300

    master: 64 Optimizing Cache Management

--- public/sw.js
+++ public/sw.js
@@ -1,33 +1,37 @@
 importScripts('/src/js/polifills/serviceworker-cache-polyfill.js');
 
+const CACHE_STATIC_NAME = 'static-v2';
+const CACHE_DYNAMIC_NAME = 'dynamic-v2';
+const CACHE_STATIC_FILES_LIST = [
+	'/',
+	'/index.html',
+
+	'/src/js/app.js',
+	'/src/js/feed.js',
+	'/src/js/polifills/promise.js',
+	'/src/js/polifills/fetch.js',
+	'/src/js/polifills/serviceworker-cache-polyfill.js',
+	'/src/js/material.min.js',
+
+	'/src/css/app.css',
+	'/src/css/feed.css',
+
+	'/src/images/main-image.jpg',
+
+	'https://fonts.googleapis.com/css?family=Roboto:400,700',
+	'https://fonts.googleapis.com/icon?family=Material+Icons',
+	'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+];
+
 // https://developer.mozilla.org/en-US/docs/Web/API/InstallEvent
 self.addEventListener('install', event => {
 	console.log('[Service Worker] Install Service Worker ...', event);
 
 	event.waitUntil(
-		caches.open('static')
+		caches.open(CACHE_STATIC_NAME)
 			.then(cache => {
 				console.log('[Service Worker] Precaching App Shell');
-				cache.addAll([
-					'/',
-					'/index.html',
-
-					'/src/js/app.js',
-					'/src/js/feed.js',
-					'/src/js/polifills/promise.js',
-					'/src/js/polifills/fetch.js',
-					'/src/js/polifills/serviceworker-cache-polyfill.js',
-					'/src/js/material.min.js',
-
-					'/src/css/app.css',
-					'/src/css/feed.css',
-
-					'/src/images/main-image.jpg',
-
-					'https://fonts.googleapis.com/css?family=Roboto:400,700',
-					'https://fonts.googleapis.com/icon?family=Material+Icons',
-					'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
-				]);
+				cache.addAll(CACHE_STATIC_FILES_LIST);
 			})
 			.catch(e => {
 				console.error(e)
@@ -42,7 +46,7 @@ self.addEventListener('activate', event => {
 	event.waitUntil(
 		caches.keys()
 			.then(keyList => Promise.all(keyList.map(key => {
-				if (key !== 'static' && key !== 'dynamic') {
+				if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
 					console.log('[Service Worker] Remove old cache ...', key);
 					return caches.delete(key);
 				}
@@ -57,7 +61,7 @@ self.addEventListener('fetch', event => {
 	event.respondWith(
 		caches.match(event.request)
 			.then(res_1 => res_1 ? res_1 : fetch(event.request)
-				.then(res_2 => caches.open('dynamic')
+				.then(res_2 => caches.open(CACHE_DYNAMIC_NAME)
 					.then(cache => {
 						cache.put(event.request.url, res_2.clone());
 						return res_2;
